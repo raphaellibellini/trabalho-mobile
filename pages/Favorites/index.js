e96def12b54707dd7b6d5fbf8b125ddfe4927482
raphaellibellini <rafael.bellini@hotmail.com>
2021-05-02 12:03:38 -0300

    Criar header página favoritos

--- pages/Favorites/index.js
+++ pages/Favorites/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { Card, Button } from 'react-native-elements'
 import Constants from 'expo-constants';
 import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
 export default function Favorites() {
     const navigation = useNavigation();
@@ -13,35 +14,45 @@ export default function Favorites() {
 
     return (
         <View style={{backgroundColor: '#EFEEF5'}}>
-        <FlatList
-            data={[
-            {id:0, full_name: 'Spider-Man', secret_name: 'Miles Morales', image: require('../../images/miles.jpg')},
-            {id:1, full_name: 'Batman', secret_name: 'Bruce Wayne', image: require('../../images/bruceWayne.jpeg')},
-            {id:2, full_name: 'Ms. Marvel', secret_name: 'Kamala Khan', image: require('../../images/kamalaKhan.jpg')}
-            ]}
-            keyExtractor={item => String(item.id)} //para que cada elemento tenha uma chave unica
-            showsVerticalScrollIndicator={false} //para ocultar a barrinha
-            contentContainerStyle={{ paddingBottom: 72, paddingTop: Constants.statusBarHeight + 20 }}
-            renderItem={({item}) => (
-            <Card style={{padding: 30}}>
-                <Card.Title style={{color: '#2288DD'}}>{item.full_name}</Card.Title>
-                <Card.Divider/>
-                <Card.Title>{item.secret_name}</Card.Title>
-                <Card.Image source={item.image}>
-                </Card.Image>
-                <View style={{marginTop: 16 , flexDirection: 'row', justifyContent: 'space-between'}}>
-                <Button
-                    buttonStyle={{paddingHorizontal: 24}}
-                    onPress={navigateToSkills}
-                    type= "outline"
-                    title='Habilidades' />
-                <Button
-                    buttonStyle={{paddingHorizontal: 32}}
-                    title='Favoritar' />
-                </View>
-            </Card>
-            )}
-        />
+            <View style={{alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal:14, paddingTop: Constants.statusBarHeight + 20}}>
+                <TouchableOpacity onPress={() => {}}>
+                    <Feather name="chevron-left" size={28} />
+                </TouchableOpacity>
+                <Text style={{fontWeight: 'bold', fontSize: 16}}>Meus Favoritos</Text>
+                <TouchableOpacity onPress={() => {}}>
+                    <Feather name="log-out" size={28} />
+                </TouchableOpacity>
+            </View>
+        
+            <FlatList
+                data={[
+                {id:0, full_name: 'Spider-Man', secret_name: 'Miles Morales', image: require('../../images/miles.jpg')},
+                {id:1, full_name: 'Batman', secret_name: 'Bruce Wayne', image: require('../../images/bruceWayne.jpeg')},
+                {id:2, full_name: 'Ms. Marvel', secret_name: 'Kamala Khan', image: require('../../images/kamalaKhan.jpg')}
+                ]}
+                keyExtractor={item => String(item.id)} //para que cada elemento tenha uma chave unica
+                showsVerticalScrollIndicator={false} //para ocultar a barrinha
+                contentContainerStyle={{ paddingBottom: 120 }}
+                renderItem={({item}) => (
+                <Card style={{padding: 30}}>
+                    <Card.Title style={{color: '#2288DD', fontWeight: 'bold', fontSize:20 }}>{item.full_name}</Card.Title>
+                    <Card.Divider/>
+                    <Card.Title>{item.secret_name}</Card.Title>
+                    <Card.Image source={item.image}>
+                    </Card.Image>
+                    <View style={{marginTop: 16 , flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <Button
+                        buttonStyle={{paddingHorizontal: 24}}
+                        onPress={navigateToSkills}
+                        type= "outline"
+                        title='Habilidades' />
+                    <Button
+                        buttonStyle={{paddingHorizontal: 32}}
+                        title='Favoritar' />
+                    </View>
+                </Card>
+                )}
+            />
         </View>
     );
 }
